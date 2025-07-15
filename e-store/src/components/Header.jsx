1c7a3ad5abb0e4c022c1dbeacd0d216d2e391368
Chandan <cgupta7510@gmail.com>
2025-07-15 19:17:41 +0530

    slider section completed

--- e-store/src/components/Header.jsx
+++ e-store/src/components/Header.jsx
@@ -9,18 +9,18 @@ export default function Header() {
             <Container className={'flex items-center justify-between'}>
                 {/* Logo */}
                 <Link href='/'>
-                    <div className='border'>
+                    <div >
                         <span className='text-pink-500 font-bold text-4xl'>E-Store <b className='text-black'>.</b></span>
                     </div>
                 </Link>
                 {/* Search bar */}
-                <div className='max-w-[500px] border rounded-full flex items-center'>
+                <div className='max-w-[500px] border border-pink-400 rounded-full flex items-center'>
                     <input
-                        className='text-slate-700 outline-0 w-[100%] px-2 py-2'
+                        className='text-slate-700 w-full px-4 py-2 outline-none'
                         placeholder='Search for Products'
                         type="text" />
-                    <div className='bg-pink-600 ml-5 flex items-center justify-center '>
-                        <button className='text-white' ><FiSearch size={18} /></button>
+                    <div className='ml-5 bg-pink-600 rounded-r-full px-2 py-2 '>
+                        <button title='Click to search' className=' font-bold cursor-pointer px-3 py-2 text-white' ><FiSearch size={18} /></button>
                     </div>
                 </div>
                 <NavBar />
@@ -40,7 +40,7 @@ const NavBar = () => {
             <div>
                 <div className='relative'>
                     <FiShoppingCart size={24} />
-                    <span className='absolute top-[-10px] right-[-20px] bg-pink-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center'>3</span>
+                    <span className='absolute top-[-10px] right-[-20px] bg-pink-600  text-white text-xs rounded-full h-5 w-5 flex items-center justify-center'>3</span>
                 </div>
             </div>
 
